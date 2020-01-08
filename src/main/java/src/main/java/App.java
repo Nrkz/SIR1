30016c7f068efbe6f22fce945b9e3f4e588d84a4
16007039 <16007039@e214m02.istic.univ-rennes1.fr>
2020-01-08 16:52:41 +0100

    first commit

---
+++ src/main/java/src/main/java/App.java
@@ -0,0 +1,16 @@
+package src.main.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{	/**
+	*This is the main function, it prints out "Hello World!"
+	*/
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
